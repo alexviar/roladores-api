dfebc0889d6a185b1172fc0daa6a2ec70bdd627c
Alexander <alexander.va@hotmail.com>
2025-06-08 13:34:47 -0400

    feat: scaffold categories base structure

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::controller(CategoryController::class)
+    ->group(function () {
+        Route::get('', 'index');
+        Route::get('{category}', 'show');
+        Route::post('', 'store');
+        Route::patch('{category}', 'update');
+        Route::delete('{category}', 'destroy');
+    })
+    ->prefix('categories')
+    ->middleware('auth:sanctum');
