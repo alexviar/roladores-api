6a3bc13b9c0c36095dc1aa6894df832331dcaf38
Alexander <alexander.va@hotmail.com>
2025-06-09 06:45:13 -0400

    fix: solve incorrect routing by reordering route methods

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PunishmentController;
 use App\Http\Controllers\RentalPeriodController;
@@ -14,49 +15,50 @@ Route::controller(AuthController::class)
         Route::post('login', 'login')->withoutMiddleware('auth:sanctum');
         Route::post('logout', 'logout');
         Route::get('user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+            return $request->user();
+        });
+    });
 
 Route::controller(CategoryController::class)
+    ->prefix('categories')
+    ->middleware('auth:sanctum')
     ->group(function () {
         Route::get('', 'index');
         Route::get('{category}', 'show');
         Route::post('', 'store');
         Route::patch('{category}', 'update');
         Route::delete('{category}', 'destroy');
-    })
-    ->prefix('categories')
-    ->middleware('auth:sanctum');
+    });
 
 Route::controller(RoladorController::class)
+    ->prefix('roladores')
+    ->middleware('auth:sanctum')
     ->group(function () {
         Route::get('', 'index');
         Route::get('{rolador}', 'show');
         Route::post('', 'store');
         Route::patch('{rolador}', 'update');
         Route::delete('{rolador}', 'destroy');
-    })
-    ->prefix('roladores')
-    ->middleware('auth:sanctum');
+    });
 
 Route::controller(PunishmentController::class)
+    ->prefix('punishments')
+    ->middleware('auth:sanctum')
     ->group(function () {
         Route::get('', 'index');
         Route::get('{punishment}', 'show');
         Route::post('', 'store');
         Route::patch('{punishment}', 'update');
         Route::delete('{punishment}', 'destroy');
-    })
-    ->prefix('punishments')
-    ->middleware('auth:sanctum');
+    });
 
 Route::controller(RentalPeriodController::class)
+    ->prefix('punishments')
+    ->middleware('auth:sanctum')
     ->group(function () {
         Route::get('', 'index');
         Route::get('{rentalPeriod}', 'show');
         Route::post('', 'store');
         Route::patch('{rentalPeriod}', 'update');
         Route::delete('{rentalPeriod}', 'destroy');
-    })
-    ->prefix('punishments')
-    ->middleware('auth:sanctum');
+    });
