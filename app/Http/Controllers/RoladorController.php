d0bd4508842e217f6d124e688e9a1f70ad3658e4
Alexander <alexander.va@hotmail.com>
2025-06-12 13:27:32 -0400

    feat: restrict rolador creation to admin users only

--- app/Http/Controllers/RoladorController.php
+++ app/Http/Controllers/RoladorController.php
@@ -61,6 +61,8 @@ class RoladorController extends Controller
      */
     public function store(Request $request)
     {
+        Gate::allowIf(fn(User $user) => $user->email === 'admin@plazadelvestido.com');
+
         $payload = $request->all();
 
         /** @var \Illuminate\Http\UploadedFile $photo */
